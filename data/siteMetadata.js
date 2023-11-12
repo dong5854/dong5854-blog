d2a899739b914a4fe492c6fbe83bd245382458e4
dong young <dong5854@gmail.com>
2023-11-12 21:54:53 +0900

    feat: edit siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'tech-blog',
+  title: `dong5854's blog`,
   author: 'Dong Young Lee',
-  headerTitle: 'dong5854 blog',
-  description: '기술 블로그',
+  headerTitle: `dong5854's blog`,
+  description: `dong5854's 블로그`,
   language: 'ko-KR',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tech-blog-lac-one.vercel.app/',
+  siteUrl: 'https://dong5854-blog-lac-one.vercel.app/',
   siteRepo: 'https://github.com/dong5854/tech-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'dong5854@gmail.com',
+  github: 'https://github.com/dong5854',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/%EB%8F%99%EC%98%81-%EC%9D%B4-a88b7b244/',
   locale: 'ko-KR',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
