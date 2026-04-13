198f8dc8ffe9b2b828787303d51eb934d64a7455
dong young <dong5854@gmail.com>
2026-04-13 23:33:28 +0900

    fix: dev 모드에서 MDX 변경 시 자동 반영되도록 개선
    velite watch를 concurrently로 별도 프로세스로 분리하고,
    contentlayer에서 dev 모드일 때 Proxy를 사용하여 요청마다
    .velite/ JSON을 다시 읽도록 변경한다.

--- lib/contentlayer.ts
+++ lib/contentlayer.ts
@@ -53,7 +53,36 @@ function readVeliteJson<T>(filename: string): T {
   return JSON.parse(readFileSync(filePath, 'utf8')) as T
 }
 
-export const allBlogs = readVeliteJson<Blog[]>('blogs.json')
-export const allAuthors = readVeliteJson<Authors[]>('authors.json')
+const isDev = process.env.NODE_ENV === 'development'
+
+function createDevProxy<T extends object>(filename: string): T {
+  const handler: ProxyHandler<T> = {
+    get(_, prop, receiver) {
+      const data = readVeliteJson<T>(filename)
+      return Reflect.get(data, prop, receiver)
+    },
+    has(_, prop) {
+      const data = readVeliteJson<T>(filename)
+      return Reflect.has(data, prop)
+    },
+    ownKeys() {
+      const data = readVeliteJson<T>(filename)
+      return Reflect.ownKeys(data)
+    },
+    getOwnPropertyDescriptor(_, prop) {
+      const data = readVeliteJson<T>(filename)
+      return Reflect.getOwnPropertyDescriptor(data, prop)
+    },
+  }
+  return new Proxy([] as unknown as T, handler)
+}
+
+export const allBlogs: Blog[] = isDev
+  ? createDevProxy<Blog[]>('blogs.json')
+  : readVeliteJson<Blog[]>('blogs.json')
+
+export const allAuthors: Authors[] = isDev
+  ? createDevProxy<Authors[]>('authors.json')
+  : readVeliteJson<Authors[]>('authors.json')
 
 export type { Blog, Authors }
